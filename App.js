00958cc3c9484a88a129284f0451fce87e0758fe
Alicia <alicia@ondibs.com>
2020-11-16 23:28:23 -0500

    Created a class to display blank page
    Next step = style the text to see if I can get it to show up

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello World!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+/**
+ * @class App
+ * @extends Component
+ */
+class App extends Component {
+  /**
+   * @constructor
+   * @constructs App
+   */
+  constructor() {
+    super();
+    
+  }
+  render() {
+    console.log('testing here');
+    return (
+      <View>
+        <Text>Alicia App</Text>
+      </View>
+    );
+  }
+}
+export default App;
