60e0266ef30886a0417e29ed10bcc90a25e6be3f
Alicia <alicia@ondibs.com>
2020-11-20 15:47:02 -0500

    Registering load then redirecting to login page

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Font from 'expo-font';
+import styled from 'styled-components';
 
 import store from './app/store';
 import Config from './config.json';
 import LandingPage from './app/components/LandingPage';
+import LinearLoader from './app/components/shared/LinearLoader';
 import EnterEmail from './app/components/AuthPage/EnterEmail';
 
 import {
@@ -22,17 +25,12 @@ import {
   setStudio,
 } from './app/actions';
 
-// update packages and start from scratch - only what we need
-// set up homepage
-// set up authorization -> take the homepage when successful
-// determine which screen to show next (login or homepage based on usertoken)
-// set the left side menu
-// set myAccount
-// set up book
-// set myCalendar
-// make first screen the splash image
-// set up loader
-
+const StyledLoadingPage = styled.View`
+  align-items: center;
+  background: ${Config.STUDIO_COLOR};
+  justify-content: center;
+  flex: 5;
+`;
 
 const Stack = createStackNavigator();
 
@@ -48,13 +46,69 @@ class App extends React.Component {
       fetchedAssets: false,
       errorOccurred: false,
       userToken: null,
+      fontLoaded: false,
+      imageLoaded: false,
     }
   }
+  // async _loadFontsAsync() {
+  //   await Font.loadAsync(customFonts);
+  //   this.setState({ fontLoaded: true });
+  // }
   /**
    * @returns {undefined}
    */
   async componentDidMount() {
+    this.getFonts();
     await this.getAssets();
+  }
+  /**
+   * @returns {undefined}
+   */
+  async getFonts() {
+    try {
+      const fonts = {
+        'studio-font': require('./assets/fonts/Regular.ttf'),
+        'studio-font-heavy': require('./assets/fonts/Bold.ttf'),
+      };
+      await Font.loadAsync(fonts);
+      this.setState({ fontLoaded: true });
+    } catch(err) {
+      console.log('something happening here');
+      console.log(err);
+    } 
+  }
+  /**
+   * @returns {undefined}
+   */
+  async getImages() {
+    await Asset.loadAsync([
+      MainPage,
+      ActivityGrey,
+      ActivityWhite,
+      CalendarGrey,
+      CalendarWhite,
+      CartGrey,
+      CartWhite,
+      MainGrey,
+      Amex,
+      Diners,
+      Discover,
+      JCB,
+      MasterCard,
+      Unknown,
+      Visa,
+      TrashGrey,
+      UserGrey,
+      UserWhite,
+      FilterWhite,
+      CheckWhite,
+      AddToCalendar,
+      MyClassesIcon,
+      ColumnSpotBooking,
+      DoorSpotBooking,
+    ]);
+
+    this.setState({ imageLoaded: true });
   }
   /**
    * @returns {undefined}
@@ -98,19 +152,27 @@ class App extends React.Component {
     const initialRoute = `${this.state.userToken ? LANDING_ROUTE : VERIFY_ROUTE}`;
     console.log(`initalRoute = ${initialRoute}`);
     console.log(`userToken = ${this.state.userToken}`);
+    console.log(`fonts loaded = ${this.state.fontLoaded}`);
+    // console.log(`font function: ${Font.isLoaded()}`);
 
     return (
       <Provider store={store}>
         <NavigationContainer>
-          <Stack.Navigator 
-            initialRouteName={initialRoute}
-            screenOptions={{
-              headerShown: false,
-              headerStyle: { backgroundColor: Config.STUDIO_COLOR } 
-              }}>
-            <Stack.Screen name={LANDING_ROUTE} component={LandingPage}/>
-            <Stack.Screen name={VERIFY_ROUTE} component={EnterEmail}/>
-          </Stack.Navigator>
+          {(this.state.fontLoaded) ? (
+            <Stack.Navigator 
+              initialRouteName={initialRoute}
+              screenOptions={{
+                headerShown: false,
+                headerStyle: { backgroundColor: Config.STUDIO_COLOR } 
+                }}>
+              <Stack.Screen name={LANDING_ROUTE} component={LandingPage}/>
+              <Stack.Screen name={VERIFY_ROUTE} component={EnterEmail}/>
+            </Stack.Navigator>
+          ) : (
+            <StyledLoadingPage>
+                <LinearLoader showQuote={this.state.fontLoaded} />
+              </StyledLoadingPage>
+          )}
         </NavigationContainer>
       </Provider>
     );
