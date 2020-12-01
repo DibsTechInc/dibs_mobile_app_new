17905c8afc2112e544fd377ac0f9c7782607e240
Alicia <alicia@ondibs.com>
2020-12-01 11:32:01 -0500

    Enter email component established

--- app/components/AuthPage/EnterEmail.js
+++ app/components/AuthPage/EnterEmail.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
+import React, { PureComponent } from 'react';
 import { ScrollView, Keyboard } from 'react-native';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
+import Promise from 'bluebird';
+import styled from 'styled-components';
 
 import Config from '../../../config.json';
 import {
@@ -15,9 +17,18 @@ import { validateEmail } from '../../actions/UserActions';
 import {
   LANDING_ROUTE,
 } from '../../constants/RouteConstants';
+import { KeyboardAccessoryView } from 'react-native-keyboard-accessory';
+import { DEFAULT_BG } from '../../constants';
+
+const StyledButtonView = styled.View`
+  padding: 8px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
 
 
-class EnterEmail extends Component {
+class EnterEmail extends PureComponent {
   /**
      * @constructor
      * @cosntructs LandingPage
@@ -25,23 +36,98 @@ class EnterEmail extends Component {
      */
     constructor(props) {
       super(props);
+      this.state = {
+        email: '',
+        isLoading: false,
+        errorText: '',
+        validInput: false,
+      };
+      this.handleOnPress = this.handleOnPress.bind(this);
+      this.onChangeText = this.onChangeText.bind(this);
     }
+
+    /**
+     * @returns {undefined}
+     */
+    componentWillUnmount() {
+      Keyboard.dismiss();
+    }
+    /**
+     * @param {string} email - user email
+     * @returns {undefined}
+     */
+    onChangeText(email) {
+      this.setState({ email });
+    }
+    /**
+     * @returns {undefined}
+     */
+    async handleOnPress() {
+      const { email } = this.state;
+
+      console.log(`state of this: ${JSON.stringify(this.state)}`);
+
+      // Heavy duty regex catches most email formatting errors
+      const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const isValidEmail = validEmail.test(email);
+
+      console.log(`validEmail = ${validEmail}`);
+      console.log(`isValidEmail = ${isValidEmail}`);
+
+      if (!isValidEmail) {
+        this.setState({ errorText: 'Please enter a valid email address.' });
+        return;
+      }
+
+      await new Promise(res => this.setState({ isLoading: true, validInput: true }, res));
+      const route = await this.props.validateEmail(email);
+      
+      if (!route) {
+        this.setState({ isLoading: false, errorText: 'Please enter a valid email address.'});
+        this.props.screenProps.isLoading = false;
+      } else {
+        this.props.navigation.navigate(route, { email, fromReset: false });
+      }
+   
+    }
+
+    /**
+     * @returns {JSX} XML
+     */
+
     render() {
       console.log('I am in the verify route');
       return (
           <FadeInView>
             <ScrollView keyboardShouldPersistTaps="always" contentContainerStyle={{ justifyContent: 'center', alignItems: 'center', height: '70%', position: 'relative'}}>
-            <InputField
-              customFocus
-              label="What's your email?"
-              autoCapitalize="none"
-              containerStyle={{
-                marginBottom: 50,
-                width: 200,
-              }}
-              labelStyle={{ marginBottom: 20, textAlign: 'center' }}
-            />
+              <InputField
+                customFocus
+                label="What's your email?"
+                onChangeText={this.onChangeText}
+                onSubmitEditing={this.handleOnPress}
+                value={this.state.email}
+                autoCapitalize="none"
+                containerStyle={{
+                  marginBottom: 50,
+                  width: 200,
+                }}
+                labelStyle={{ marginBottom: 20, textAlign: 'center' }}
+                blurOnSubmit={this.state.validInput}
+              />
             </ScrollView>
+            <KeyboardAccessoryView
+            alwaysVisible
+            hideBoarder
+            style={{ backgroundColor: DEFAULT_BG, margineBottom: 25 }}
+            >
+              <StyledButtonView>
+                <MaterialButton
+                  onPress={this.handleOnPress}
+                  text="Next"
+                  style={{ width: '75%', height: 40 }}
+                />
+              </StyledButtonView>
+            </KeyboardAccessoryView>
           </FadeInView>
       );
 
@@ -52,9 +138,9 @@ EnterEmail.propTypes = {
   navigation: PropTypes.shape(),
 };
 
-EnterEmail.navigationOptions = {
-  headerMode: 'none',
-};
+// EnterEmail.navigationOptions = {
+//   headerMode: 'none',
+// };
 
 const mapDispatchToProps = {
   validateEmail,
