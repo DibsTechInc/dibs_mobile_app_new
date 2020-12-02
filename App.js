ab4b78280a8a8ff59b4f349984a8baafd0779a4d
Alicia <alicia@ondibs.com>
2020-12-02 16:05:18 -0500

    Stack navigation

--- App.js
+++ App.js
@@ -17,12 +17,28 @@ import LinearLoader from './app/components/shared/LinearLoader';
 import EnterEmail from './app/components/AuthPage/EnterEmail';
 import EnterPassword from './app/components/AuthPage/EnterPassword';
 import MainPage from './app/components/MainPage';
+import TermsAndConditions from './app/components/AuthPage/TermsAndConditions';
+import UserSettings from './app/components/ProfilePage/UserSettings';
+import EditEmail from './app/components/ProfilePage/EditEmail';
+import EditUserName from './app/components/ProfilePage/EditUserName';
+import EditCC from './app/components/ProfilePage/EditCC';
+import EditPassword from './app/components/ProfilePage/EditPassword';
+import FAQ from './app/components/ProfilePage/FAQ';
+import ProfilePage from './app/components/ProfilePage/ProfilePage';
 
 import {
   LANDING_ROUTE,
   VERIFY_ROUTE,
   LOGIN_ROUTE,
   MAIN_ROUTE,
+  SETTINGS_ROUTE,
+  TERMS_AND_CONDITIONS_ROUTE,
+  EDIT_USERNAME_ROUTE,
+  EDIT_EMAIL_ROUTE,
+  FAQ_ROUTE,
+  EDIT_PASSWORD_ROUTE,
+  EDIT_CC_ROUTE,
+  PROFILE_ROUTE,
 } from './app/constants/RouteConstants';
 
 import {
@@ -225,6 +241,14 @@ class App extends React.Component {
               <Stack.Screen name={VERIFY_ROUTE} component={EnterEmail}/>
               <Stack.Screen name={LOGIN_ROUTE} component={EnterPassword}/>
               <Stack.Screen name={MAIN_ROUTE} component={MainPage}/>
+              <Stack.Screen name={SETTINGS_ROUTE} component={UserSettings}/>
+              <Stack.Screen name={FAQ_ROUTE} component={FAQ}/>
+              <Stack.Screen name={PROFILE_ROUTE} component={ProfilePage}/>
+              <Stack.Screen name={TERMS_AND_CONDITIONS_ROUTE} component={TermsAndConditions}/>
+              <Stack.Screen name={EDIT_EMAIL_ROUTE} component={EditEmail}/>
+              <Stack.Screen name={EDIT_USERNAME_ROUTE} component={EditUserName}/>
+              <Stack.Screen name={EDIT_CC_ROUTE} component={EditCC}/>
+              <Stack.Screen name={EDIT_PASSWORD_ROUTE} component={EditPassword}/>
             </Stack.Navigator>
           ) : (
             <StyledLoadingPage>
