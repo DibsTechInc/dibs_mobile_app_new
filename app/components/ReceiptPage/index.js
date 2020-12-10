af9837cf448e58e2b0c76df886f233bd37d18626
Alicia <alicia@ondibs.com>
2020-12-10 15:46:30 -0500

    props seem ok

--- app/components/ReceiptPage/index.js
+++ app/components/ReceiptPage/index.js
@@ -1,4 +1,5 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getDetailedConfirmationEvents, getDetailedConfirmationPackages, getDetailedConfirmationCredits } from '../../selectors';
@@ -10,7 +11,15 @@ import Header from '../Header';
  * @class ReceiptPage
  * @extends {Component}
  */
-class ReceiptPage extends PureComponent {
+class ReceiptPage extends Component {
+  /**
+   * @constructor
+   * @constructs ReceiptPage
+   * @param {Object} props for component
+   */
+  constructor(props) {
+    super(props);
+  }
   /**
    * @returns {JSX} XML
    */
@@ -21,17 +30,29 @@ class ReceiptPage extends PureComponent {
    * @returns {JSX} XML
    */
   render() {
+    console.log(`\n\n\nDO I MAKE IT TO THE RECEIPT PAGE EVER?`);
+    // next up --> test whether the props are good
+    // then bring in the fadeinview and all of the other stuff
     return (
-      <FadeInView>
-        <Header title="Order Summary" />
-        <PaginatedSlider
-          forReceiptPage
-          credits={this.props.credits}
-          packages={this.props.packages}
-          events={this.props.events}
-        />
-      </FadeInView>
-    );
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#6fbf98'}}>
+        <Text>This is the receipt page</Text>
+        <Button 
+          title="Click here to go to the email page"
+          onPress={() => this.props.navigation.navigate(VERIFY_ROUTE)}
+          />
+      </View>
+  );
+    // return (
+    //   <FadeInView>
+    //     <Header title="Order Summary" />
+    //     <PaginatedSlider
+    //       forReceiptPage
+    //       credits={this.props.credits}
+    //       packages={this.props.packages}
+    //       events={this.props.events}
+    //     />
+    //   </FadeInView>
+    // );
   }
 }
 
@@ -43,6 +64,7 @@ ReceiptPage.propTypes = {
 };
 
 const mapStateToProps = state => ({
+  // testing: 1,
   credits: getDetailedConfirmationCredits(state),
   packages: getDetailedConfirmationPackages(state),
   events: getDetailedConfirmationEvents(state),
